f08482ced5351298cf03b5c8bc61d95bd6901ee5
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-17 14:21:06 +0200

    Container: Force Single Objects, Celebrate

--- lib/Container.php
+++ lib/Container.php
@@ -2,18 +2,55 @@
 
 class Container
 {
+    private $pdo;
+
+    private $shipLoader;
+
+    private $battleManager;
+
     public function __construct(private array $configuration)
     {
     }
 
+    /**
+     * @return PDO
+     */
     public function getPDO()
     {
-        $pdo = new PDO(
-            $this->configuration['db_dsn'],
-            $this->configuration['db_user'],
-            $this->configuration['db_pass'],
-        );
+        if (null === $this->pdo) {
+            $this->pdo = new PDO(
+                $this->configuration['db_dsn'],
+                $this->configuration['db_user'],
+                $this->configuration['db_pass'],
+            );
 
-        return $pdo;
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return $this->pdo;
+    }
+
+    /**
+     * @return ShipLoader
+     */
+    public function getShipLoader()
+    {
+        if (null === $this->shipLoader){
+            $this->shipLoader = new ShipLoader($this->getPDO());
+        }
+
+        return $this->shipLoader;
+    }
+
+    /**
+     * @return BattleManager
+     */
+    public function getBattleManager()
+    {
+        if (null === $this->battleManager) {
+            $this->battleManager = new BattleManager();
+        }
+
+        return $this->battleManager;
     }
 }
