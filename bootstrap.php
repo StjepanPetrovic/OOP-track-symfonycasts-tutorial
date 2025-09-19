109d1c3f3e1cebda78b4614eb44dcff103b49de1
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-19 16:01:29 +0200

    Obtain the EntityManager

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,27 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require __DIR__ . '/vendor/autoload.php';
+
 $configuration = array(
     'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
     'db_user' => 'root',
     'db_pass' => null,
 );
 
-require __DIR__ . '/vendor/autoload.php';
+$dbParamsDoctrine = [
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => '',
+    'dbname'   => 'oo_battle',
+];
+
+$configDoctrine = ORMSetup::createAttributeMetadataConfig(
+    paths: [__DIR__ . '/lib'],
+    isDevMode: true,
+);
+$connection = DriverManager::getConnection($dbParamsDoctrine, $configDoctrine);
+$entityManager = new EntityManager($connection, $configDoctrine);
