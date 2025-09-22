5dcdb4bb0879a398ab270661861273421432c84c
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-22 09:24:53 +0200

    Add initial configurations for doctrine

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
 use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -12,14 +15,37 @@ $configuration = array(
     'db_pass' => null,
 );
 
-$dbParamsDoctrine = [
-    'driver' => 'pdo_sqlite',
-    'path' => __DIR__ . '/db.sqlite',
-];
+$applicationMode = 'development';
 
-$configDoctrine = ORMSetup::createAttributeMetadataConfig(
-    paths: [__DIR__ . '/lib'],
-    isDevMode: true,
-);
-$connection = DriverManager::getConnection($dbParamsDoctrine, $configDoctrine);
-$entityManager = new EntityManager($connection, $configDoctrine);
+if ($applicationMode === "development") {
+    $queryCache = new ArrayAdapter();
+    $metadataCache = new ArrayAdapter();
+} else {
+    $queryCache = new PhpFilesAdapter('doctrine_queries');
+    $metadataCache = new PhpFilesAdapter('doctrine_metadata');
+}
+
+$config = new Configuration;
+$config->setMetadataCache($metadataCache);
+$driverImpl = new AttributeDriver([__DIR__ . '/lib/Entity'], true);
+$config->setMetadataDriverImpl($driverImpl);
+$config->setQueryCache($queryCache);
+
+if (PHP_VERSION_ID > 80400) {
+    $config->enableNativeLazyObjects(true);
+} else {
+    $config->setProxyDir(__DIR__ . '/lib/Proxies');
+    $config->setProxyNamespace('Proxies');
+    if ($applicationMode === "development") {
+        $config->setAutoGenerateProxyClasses(true);
+    } else {
+        $config->setAutoGenerateProxyClasses(false);
+    }
+}
+
+$connection = DriverManager::getConnection([
+    'driver' => 'pdo_sqlite',
+    'path' => 'database.sqlite',
+], $config);
+
+$em = new EntityManager($connection, $config);
